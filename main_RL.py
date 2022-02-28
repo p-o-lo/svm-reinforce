7f7d6901d787d3054fbc7b18a830ddb224402f51
Paolo Recchia <paolo@uccio.inphyni.cnrs.fr>
2022-02-28 16:38:58 +0100

    Update main_RL and corrected SVMenv in branch uccio

--- main_RL.py
+++ main_RL.py
@@ -10,55 +10,62 @@ env = gym.make('svm_env:svmEnv-v0', file_sigmas = "./svmCodeSVD/sigmas.dat")
 # Instance of the ddpg agent
 agent = Agent(1, 3, random_seed=2)
 
-def save_all(agent, scores, last_energies, princip_dim):
+def save_all(agent, all_rewards, energies, princip_dims):
     torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
     torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
 
-    name_scores = 'scores_RL.out'
+    name_rewards = 'rewards_RL.out'
     file_scores = open(name_scores,'w')
     np.savetxt(file_scores, scores, fmt="%f")
     file_scores.close()
 
-    name_energies = 'energies_RL.out'
+    name_energies = 'energies_RL_0.out'
     file_energies = open(name_energies,'w')
     np.savetxt(file_energies, last_energies, fmt="%f")
     file_energies.close()
 
-    name_dim = 'princip_dim_RL.out'
+    name_dim = 'princip_dims_RL.out'
     file_dim = open(name_dim,'w')
     np.savetxt(file_dim, princip_dim, fmt="%f")
     file_dim.close()
 
 def run_ddpg(max_t_step = 300, n_episodes=700):
     ##Inizialization
-    scores = []
-    last_energies = []
-    princip_dim = []
+    rewards = []
+    energies = []
+    princip_dims = []
+
     for i_episode in range(1, n_episodes+1):
         state = env.reset()
         agent.reset()                  
-        score = 0.0
+        rew_per_i_episode = []
+        energies_per_i_episode = []
+        princip_dim_per_i_episode = []
 
         ## Training loop of each episode
         for t_step in range(max_t_step):
             action = agent.act(state)
             next_state, reward, done, info = env.step(action)                   
             agent.step(state, action, reward, next_state, done)
-            score += reward
-            state = next_state  
+            state = next_state
+            
+            # Save
+            rew_per_i_episode.append(reward)
+            energies_per_i_episode.append(state[0])
+            princip_dim_per_i_episode.append(env.princp_dim)
             if done:
                 break
 
         print('Episode {} ... Score: {:.3f}'.format(i_episode, score))
         
-        scores.append(score)
-        last_energies.append(state[0])
-        princip_dim.append(env.princp_dim)
+        rewards.append(rew_per_i_episode)
+        energies.append(energies_per_i_episode)
+        princip_dims.append(princip_dim_per_i_episode)
 
         ## Save data during training (to not lose the work done)
-        save_all(agent, scores, last_energies, princip_dim)
+        save_all(agent, rewards, energies, princip_dims)
 
-    return scores, last_energies, princip_dim
+    return rewards, energies, princip_dims
 
 
-scores, last_energies, princip_dim = run_ddpg()
+rewards, energies, princip_dims = run_ddpg()
