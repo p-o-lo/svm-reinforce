d94e8e52f75b06f6ac97be50c7fbccec4fb512bc
Paolo Recchia <paolo.recchia@inphyni.cnrs.fr>
2022-04-04 17:17:40 +0200

    Modified actor critic NN for ddpg

--- actor_critic_model_ddpg.py
+++ actor_critic_model_ddpg.py
@@ -11,7 +11,7 @@ def hidden_init(layer):
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=128, fc2_units=64):
+    def __init__(self, state_size, action_size, seed, fc1_units=64, fc2_units=64):
         """Initialize parameters and build model.
         Params
         ======
@@ -43,7 +43,7 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fcs1_units=128, fc2_units=64):
+    def __init__(self, state_size, action_size, seed, fcs1_units=64, fc2_units=64):
         """Initialize parameters and build model.
         Params
         ======
